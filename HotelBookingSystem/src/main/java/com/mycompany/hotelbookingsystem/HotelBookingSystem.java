be48f93b9dabe300e6d8f07d193655c4dc347bb8
Min Thiha Ko Ko <minthihakoko@gmail.com>
2024-08-05 14:29:25 +1200

    Testing new printline

--- HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/HotelBookingSystem.java
+++ HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/HotelBookingSystem.java
@@ -12,5 +12,6 @@ public class HotelBookingSystem {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hotel Booking System!");
     }
 }
