498299b53940e80be8453e535661d85755334a3d
Alex Smokina <rtk6661autuni.ac.nz>
2024-08-05 15:15:35 +1200

    Guess Class

---
+++ HotelBookingSystem/src/main/java/MainHotel/Guest.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainHotel;
+
+/**
+ *
+ * @author alex
+ */
+public class Guest implements ID {
+
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Guest(String name, String phone, String email) {
+
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.id = idGenerator();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String idGenerator() {
+        String output = "GS-";
+        int id = (int) (Math.random() * 1001);
+        return output + id;
+    }
+
+}
---
+++ HotelBookingSystem/src/main/java/MainHotel/Staff.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainHotel;
+
+/**
+ *
+ * @author alex
+ */
+public class Staff implements ID{
+    
+    
+    @Override
+    public String idGenerator() {
+        String output = "STF-";
+        int id = (int) (Math.random() * 1001);
+        return output + id;
+    }
+}
